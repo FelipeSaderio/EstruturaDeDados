c4d45d3e121d14de89488521980faedad60eb2df
Gabriel <gabrielsantana7791@gmail.com>
2022-09-01 21:36:34 -0300

    Aula sobre Array

---
+++ EstruturaDeDados/EstruturaDeDadosArray.java
@@ -0,0 +1,18 @@
+package EstruturaDeDados;
+
+public class EstruturaDeDadosArray {
+	
+	public static void main (String[] args) {
+		String nomes[] = new String[3];
+		
+		nomes[0] = "Felipe";
+		nomes[1] = "Mariana";
+		
+		System.out.println(nomes[0]);
+		System.out.println(nomes[2]);
+		System.out.println(nomes[1]);
+		System.out.println(nomes[3]);
+		
+	}
+
+}
