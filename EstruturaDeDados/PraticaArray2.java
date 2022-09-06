fc9a62dbdd85881de14c80012a24195de3c80c38
Gabriel <gabrielsantana7791@gmail.com>
2022-09-06 11:33:56 -0300

    add PraticaArray2

---
+++ EstruturaDeDados/PraticaArray2.java
@@ -0,0 +1,11 @@
+package EstruturaDeDados;
+
+public class PraticaArray2 {
+	/* Criar array com 3 variaveis de números inteiros
+	 * A primeira deve ser o número 3
+	 * A segundo deve ser o número 5
+	 * A terceira deve ser a soma da primeira e da segunda
+	 * No final o programa deve printar na tela uma mensagem com a terceira variável do array
+	 */
+
+}
