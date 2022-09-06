c528772f0f95fbabe2afc050e7f192fbd80aef23
FelipeSaderio <felipesaderio1999@gmail.com>
2022-09-05 22:19:50 -0300

    Pratica array

---
+++ EstruturaDeDados/PraticaArray.java
@@ -0,0 +1,23 @@
+package EstruturaDeDados;
+
+public class PraticaArray {
+	public static void main (String[] args) {
+		String carros[] = new String[5];
+		carros[0] = "Fusca";
+		carros[1] = "Ford";
+		carros[2] = "Fiat";
+		carros[3] = "Strada";
+		
+		String mensagem = "esses são os nossos carros: ";
+		
+		/*System.out.println( mensagem + carros[0] + " \n" +  carros[1]
+				+ " \n" + carros[2] + " \n" + carros[3]);*/
+		
+		System.out.println(carros[0]);
+		System.out.println(carros[1]);
+		System.out.println(carros[2]);
+		System.out.println(carros[3]);
+		
+	}
+
+}
